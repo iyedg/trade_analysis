916f5a8dc342160d55ef180be68bc227f4c02109
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-06-15 22:07:21 +0100

    new api, seems to work

--- trade_policy_analysis/client.py
+++ trade_policy_analysis/client.py
@@ -8,122 +8,104 @@ from urllib.parse import urljoin
 
 @attr.s
 class OEC(object):
-    classification = attr.ib()
-    _flow = attr.ib(init=False, default="import")
+    _classification = attr.ib(default="hs07", converter=str.lower)
+    _flow = attr.ib(default="import", converter=str.lower)
+    _origin = attr.ib(default="all", converter=str.lower)
+    _destination = attr.ib(default="all", converter=str.lower)
+    _products = attr.ib(default="show", converter=str.lower)
+    _year = attr.ib(default=2017, converter=int)
+    # !year 0 should be considered as None
+    _end_y = attr.ib(default=0, converter=int)
+    # TODO: use Enums instead
+    _depth = attr.ib(default=1, converter=int)
     base_uri = "https://atlas.media.mit.edu"
+
+    @_classification.validator
+    def __valid_classification(self, attribute, value):
+        # ! Assumes str.lower converter
+        classifications = ["sitc", "hs92", "hs96", "hs02", "hs07"]
+        if value not in classifications:
+            raise ValueError(f"{value} is not in {classifications}")
 
     @_flow.validator
     def __valid_flow(self, attribute, value):
+        # ! Assumes str.lower converter
         valid_flows = ["import", "export"]
         if value not in valid_flows:
             raise ValueError(f"{value} is not in {valid_flows}")
 
-    @classification.validator
-    def __valid_classification(self, attribute, value):
-        classifications = ["SITC", "HS92", "HS96", "HS02", "HS07"]
-        if value not in classifications:
-            raise ValueError(f"{value} is not in {classifications}")
-
     @property
     def flow(self):
-        return self._flow.lower()
+        return self._flow
 
     @flow.setter
     def flow(self, value):
+        value = value.lower()
         # TODO: "flow" does nothing, read more about it
         self.__valid_flow("flow", value)
         self._flow = value
 
     @property
-    @vcr.use_cassette("fixtures/countries.yaml", record_mode="new_episodes")
-    def countries(self):
+    @vcr.use_cassette("fixtures/countries_attrs.yaml", record_mode="new_episodes")
+    def countries_attrs(self):
         endpoint = "/attr/country/"
         r = requests.get(urljoin(self.base_uri, endpoint))
         return pd.DataFrame(json.loads(r.content)["data"])
 
     @property
-    @vcr.use_cassette("fixtures/products.yaml", record_mode="new_episodes")
-    def products(self):
+    @vcr.use_cassette("fixtures/products_attrs.yaml", record_mode="new_episodes")
+    def products_attrs(self):
         endpoint = f"/attr/{self.classification.lower()}/"
         r = requests.get(urljoin(self.base_uri, endpoint))
         return pd.DataFrame(json.loads(r.content)["data"])
 
-    def get_country_comtrade_name(self, country_id):
-        """Return COMTRADE name from 3 letter id
+    def imports(self):
+        self.flow = "import"
+        return self
 
-        Arguments:
-            country_id {str} -- three letter id of country
-        """
-        return self.countries.pipe(
-            lambda df: df.loc[df.display_id == country_id, "comtrade_name"]
-        ).values[0]
+    def exports(self):
+        self.flow = "export"
+        return self
 
-    def get_country_image(self, country_id):
-        # TODO: using values and list indexing is prone to error, better error handling
-        link = self.countries.pipe(
-            lambda df: df.loc[df.display_id == country_id, "image_link"]
-        ).values[0]
-        return link
+    def products(self, value):
+        self._products = value.lower()
+        return self
 
-    def get_product_icon(self, product_id):
-        # TODO: using values and list indexing is prone to error, better error handling
-        return urljoin(
-            self.base_uri,
-            self.products.pipe(lambda df: df.loc[df.id == product_id, "icon"]).values[
-                0
-            ],
+    def origin(self, value):
+        # value can be country id, show, or all
+        self._origin = value.lower()
+        return self
+
+    def destination(self, value):
+        self._destination = value.lower()
+        return self
+
+    def during(self, start_y, end_y=0):
+        # year is start_y, optionally define range with end_y
+        # TODO validation end_y must be larger than year
+        self._year = start_y
+        self._end_y = end_y
+        return self
+
+    def depth(self, value):
+        self._depth = value
+        return self
+
+    @vcr.use_cassette("fixtures/calls.yaml", record_mode="new_episodes")
+    def call(self):
+        # At most one of `origin` `destination` and
+        # `product` can have a value of "show"
+        uri = (
+            f"http://atlas.media.mit.edu/{self._classification}"
+            f"/{self._flow}/{self._year}/{self._origin}"
+            f"/{self._destination}/{self._products}/"
         )
-
-    def get_product_color(self, product_id):
-        # TODO: using values and list indexing is prone to error, better error handling
-        return self.products.pipe(
-            lambda df: df.loc[df.id == product_id, "color"]
-        ).values[0]
-
-    @vcr.use_cassette("fixtures/exports.yaml", record_mode="new_episodes")
-    def get_exports(
-        self,
-        origin="all",
-        destination="all",
-        product="all",
-        depth="section",
-        year=None,
-        start_y=None,
-        end_y=None,
-    ):
-        # TODO: lower case flow
-        if year:
-            uri = f"/{self.classification}/export/{year}/{origin}/{destination}/{product}/"
-        else:
-            uri = f"/{self.classification}/export/{start_y}.{end_y}/{origin}/{destination}/{product}/"
-        print(urljoin(self.base_uri, uri).lower())
-        r = requests.get(urljoin(self.base_uri, uri).lower())
-        df = (
-            pd.DataFrame(json.loads(r.content)["data"])
-            .pipe(lambda df: df.loc[df["hs07_id_len"] == 8])
-            .pipe(
-                lambda df: df.drop(
-                    columns=[
-                        "export_val_growth_pct",
-                        "export_val_growth_pct_5",
-                        "export_val_growth_val",
-                        "export_val_growth_val_5",
-                        "import_val_growth_pct",
-                        "import_val_growth_pct_5",
-                        "import_val_growth_val",
-                        "import_val_growth_val_5",
-                        "hs07_id_len",
-                    ]
-                )
+        if self._end_y > 0:
+            uri = (
+                f"http://atlas.media.mit.edu/{self._classification}"
+                f"/{self._flow}/{self._year}.{self._end_y}/{self._origin}"
+                f"/{self._destination}/{self._products}/"
             )
-            .pipe(
-                lambda df: df.assign(
-                    hs07_section=df["hs07_id"].str[:2],
-                    hs07_chapter=df["hs07_id"].str[2:4],
-                    hs07_heading=df["hs07_id"].str[4:6],
-                    hs07_subheading=df["hs07_id"].str[6:8],
-                )
-            )
-            .fillna(0)
-        )
-        return df
+        print(uri)
+        response = requests.get(uri)
+        return pd.DataFrame(json.loads(response.content)["data"])
