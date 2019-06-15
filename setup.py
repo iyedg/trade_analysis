2391f3dadf81604b9786eaf016a5b4b6d8bbc786
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-06-15 08:01:23 +0100

    renamed directory

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name="trade_analysis",
+    name="trade_policy_analysis",
     version="0.1dev",
-    packages=["trade_analysis"],
+    packages=["trade_policy_analysis"],
     license="Creative Commons Attribution-Noncommercial-Share Alike license",
     long_description=open("README").read(),
 )
